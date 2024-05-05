91d4eab1f1a1f997984c0384cca02662bd4bbcf1
Nicolo <nicolo.dellepiane@mail.polimi.it>
2024-05-06 00:40:08 +0200

    proto of automount

--- bumblebee_status/modules/contrib/eject.py
+++ bumblebee_status/modules/contrib/eject.py
@@ -10,7 +10,9 @@ class Module(core.module.Module):
         super().__init__(config, theme, core.widget.Widget(self.output))
 
         self.__mountpoint = self.parameter("mountpoint", "/mnt")
-        self.__text = f"device mounted at {self.__mountpoint}"
+        self.__text = ""
+        # = f"device mounted at {self.__mountpoint}"
+        self.__bus = dbus.SystemBus()
         #self.__bus = dbus.SystemBus()
         self.clicked = False
         self.err = "not touched"
@@ -18,7 +20,7 @@ class Module(core.module.Module):
         event = {
             "type": "unmount",
             "action": self.unmount_USB,
-            "button": core.input.LEFT_MOUSE
+            "button": core.input.RIGHT_MOUSE
         }
 
         core.input.register(self, button=event["button"], cmd=event["action"])
@@ -29,10 +31,32 @@ class Module(core.module.Module):
 
         return isMounted
 
+    def mount_USB(self, widget):
+        """
+        DEBUG: viene montato a /run/media/nico/*cose*, non si vede che è montato e non si riesce a fare l'unmount. Però viene montato :)
+        """
+        ud_manager_obj = self.__bus.get_object('org.freedesktop.UDisks2', '/org/freedesktop/UDisks2')
+        ud_manager = dbus.Interface(ud_manager_obj, 'org.freedesktop.DBus.ObjectManager')
+
+        for objPath, interfaces in ud_manager.GetManagedObjects().items():
+            if \
+                'org.freedesktop.UDisks2.Filesystem' in interfaces and \
+                'sda1' in objPath:
+
+                block_obj = self.__bus.get_object('org.freedesktop.UDisks2', objPath)
+                block = dbus.Interface(block_obj, 'org.freedesktop.UDisks2.Filesystem')
+
+                try:
+                    options = dbus.Dictionary(signature='sv')
+                    self.err = block.Mount(options, )
+                except Exception as e:
+                    self.err = str(e)
+        return
+
     def unmount_USB(self, widget):
         self.clicked = True
-        bus = dbus.SystemBus()
-        ud_manager_obj = bus.get_object('org.freedesktop.UDisks2', '/org/freedesktop/UDisks2')
+        
+        ud_manager_obj = self.__bus.get_object('org.freedesktop.UDisks2', '/org/freedesktop/UDisks2')
         ud_manager = dbus.Interface(ud_manager_obj, 'org.freedesktop.DBus.ObjectManager')
 
         for objPath, interfaces in ud_manager.GetManagedObjects().items():
@@ -42,15 +66,16 @@ class Module(core.module.Module):
 
                 mountpoint = interfaces['org.freedesktop.UDisks2.Filesystem']['MountPoints'][0]
                 if bytes(mountpoint).decode('utf-8').rstrip('\x00') == self.__mountpoint:
-                    filesystem_obj = bus.get_object('org.freedesktop.UDisks2', objPath)
+                    filesystem_obj = self.__bus.get_object('org.freedesktop.UDisks2', objPath)
                     filesystem = dbus.Interface(filesystem_obj, 'org.freedesktop.UDisks2.Filesystem')
 
                     try:
                         options = dbus.Dictionary(signature='sv')
                         filesystem.Unmount(options)
-                        self.err = "done?"
+                        # DEBUG --> self.err = "done?"
                     except Exception as e:
-                        self.err = str(e)
+                        # DEBUG --> self.err = str(e)
+                        # TODO: handle error printing some red text
                         return
 
                     return
@@ -59,11 +84,18 @@ class Module(core.module.Module):
     def update(self):
         if self.clicked:
             self.__text = self.err
+        else:
+            mounted = int(self.get_mounted_USB())
+            if mounted:
+                self.__text = f"device mounted at {self.__mountpoint}"
+            else:
+                return
+                #self.mount_USB(self)
 
     def hidden(self):
         mounted = int(self.get_mounted_USB())
         if not mounted:
-            return True
+            return False
         return False
     
     def output(self, widget):
